b8247388f6b2f227c9684425ab1a848e41cb21b9
Mbognan <simpkaliw33@gmail.com>
2024-02-19 21:27:02 +0800

    The Updates

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
+    <div class="sidebar-brand d-none d-md-flex">
+        <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
+            <use xlink:href="{{ asset('admin/assets/brand/coreui.svg#full') }}"></use>
+        </svg>
+        <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
+            <image href="{{ asset('admin/assets/img/test.png') }}" width="46" height="46" />
+        </svg>
+
+    </div>
+    <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
+        <li class="nav-item"><a class="nav-link" href="index.html">
+                <svg class="nav-icon">
+                    <use xlink:href="{{ asset('admin/vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
+                </svg> Dashboard<span class="badge badge-sm bg-info ms-auto">NEW</span></a></li>
+    </ul>
+    <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
+</div>
